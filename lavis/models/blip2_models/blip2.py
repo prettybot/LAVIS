a4a7f1459b00eb39acce475e89c6d53d1016cb10
prettybot <roadlee1993@gmail.com>
2023-05-17 23:48:52 +0800

    use local bert-base-uncased

--- lavis/models/blip2_models/blip2.py
+++ lavis/models/blip2_models/blip2.py
@@ -29,7 +29,7 @@ from transformers import BertTokenizer
 class Blip2Base(BaseModel):
     @classmethod
     def init_tokenizer(cls, truncation_side="right"):
-        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", truncation_side=truncation_side)
+        tokenizer = BertTokenizer.from_pretrained("/app/models/bert-base-uncased", truncation_side=truncation_side)
         tokenizer.add_special_tokens({"bos_token": "[DEC]"})
         return tokenizer
 
